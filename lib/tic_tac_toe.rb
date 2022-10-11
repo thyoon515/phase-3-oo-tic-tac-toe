8f884ea81a22241bf79e065d3a72b170e773b0d7
thyoon515 <thyoon515@gmail.com>
2022-10-11 15:31:49 -0400

    commit tree c26768ab8a5fec3845c3df0fa011ea8bcb241727

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,114 @@
+class TicTacToe
+    attr_accessor :board
+
+    def initialize
+        @board = Array.new(9, " ")
+    end
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(string)
+        @user_input = string.to_i - 1
+    end
+
+    def move(input_to_index, player = "X")
+        @board[input_to_index] = player
+    end
+
+    def position_taken?(input_to_index)
+        if @board[input_to_index] == 'X' || @board[input_to_index] == 'O'
+            true
+        else
+            false
+        end 
+    end
+
+    def valid_move?(input_to_index)
+        !position_taken?(input_to_index) && input_to_index.between?(0,8)
+    end
+
+    def turn_count
+        count = 0
+        @board.each do |i|
+            count += 1 if i == 'X' || i == 'O'
+        end
+        count
+    end
+
+    def current_player
+        if turn_count.even?
+            return "X"
+        else turn_count.odd?
+            return "O"
+        end
+    end
+
+    def turn
+        puts "Please enter a number (1 - 9)"
+        input = gets.strip
+        position = input_to_index(input)
+        if valid_move?(position)
+            move(position, current_player)
+            display_board
+        else
+            turn
+        end
+    end
+
+    def won?
+        WIN_COMBINATIONS.detect do |combo|
+            @board[combo[0]] == @board[combo[1]] &&
+            @board[combo[1]] == @board[combo[2]] &&
+            position_taken?(combo[0])
+        end
+    end
+
+    def full?
+        @board.all? do |taken|
+            taken == "X" || taken == "O"
+        end
+    end
+
+    def draw?
+        !won? && full?
+    end
+
+    def over?
+        won? || draw?
+    end
+
+    def winner
+        if winning_combo = won?
+            @board[winning_combo.first]
+        end
+    end
+
+    def play
+        while !over?
+            turn
+        end
+        if won?
+            puts "Congratulations #{winner}!"
+        elsif draw?
+            puts "Cat's Game!"
+        end
+
+    end
+end
